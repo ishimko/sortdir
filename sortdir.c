728712f02b3c9c5a71fff2a5d08f312b117eb312
vanashimko <vana.shimko@gmail.com>
2016-03-03 13:44:38 +0300

    Error handling improvements

--- sortdir.c
+++ sortdir.c
@@ -3,6 +3,8 @@
 #include <libgen.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,12 +12,10 @@
 
 #include "constants.h"
 
-int sort
-
 
 int main(int argc, char* argv[]){
 	if (argc != ARGS_COUNT){
-		fprintf(stderr, "%s: %s", argv[0], "неверное количество аргументов\n");
+		fprintf(stderr, "%s: %s\n", argv[0], "wrong number of arguments");
 		return 1;
 	}
 	
@@ -26,8 +26,18 @@ int main(int argc, char* argv[]){
 	}
 	
 	if (~paramInfo.st_mode & S_IFDIR){
-		fprintf(stderr, "%s: %s", argv[0], "первый параметр не является каталогом\n");
+		fprintf(stderr, "%s: %s: %s\n", argv[0], "Not a directory", argv[1]);
 		return 1;
+	}
+	
+	DIR *dir;
+	dir = opendir(argv[1]);
+	if (!dir) {
+		fprintf(stderr, "%s: %s: %s\n", argv[0], strerror(errno), argv[1]);
+	}
+	
+	if (mkdir(argv[2], 0700) == -1){
+		fprintf(stderr, "%s: %s: %s\n", argv[0], strerror(errno), argv[2]);
 	}
 	
 }
