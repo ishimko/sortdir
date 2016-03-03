d75cbfdc0c4ca8285c6ace09871181312869556f
vanashimko <vana.shimko@gmail.com>
2016-03-03 13:20:58 +0300

    Some error handling

--- sortdir.c
+++ sortdir.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 #include "constants.h"
+
+int sort
 
 
 int main(int argc, char* argv[]){
 	if (argc != ARGS_COUNT){
-		fprintf(stderr, "%s: %s", basename(argv[0]), "неверное количество аргументов");
-		exit(1);
+		fprintf(stderr, "%s: %s", argv[0], "неверное количество аргументов\n");
+		return 1;
 	}
+	
+	struct stat paramInfo;
+	if (stat(argv[1], &paramInfo) == -1){
+		perror(argv[0]);
+		return 1;		
+	}
+	
+	if (~paramInfo.st_mode & S_IFDIR){
+		fprintf(stderr, "%s: %s", argv[0], "первый параметр не является каталогом\n");
+		return 1;
+	}
+	
 }
